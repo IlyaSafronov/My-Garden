466e107eea9a5462303749e28b15faca05ea1963
Ilya Safronov <IlyaSafronov18@gmail.com>
2025-03-25 01:25:14 +0300

    add all styles

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const videos = gsap.utils.toArray('.video');
+gsap.set(videos, {opacity: 0});
+gsap.registerPlugin(ScrollTrigger);
+
+videos.forEach((video) => {
+  ScrollTrigger.create ({
+    
+    trigger: video,
+    start: 'top center',
+    end: 'bottom center',
+    markers: true,
+    onEnter: () => {
+      gsap.to(video, {opacity: 1});
+      video.play()
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });  
+});
+
+
